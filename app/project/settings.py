0cab47d3cebb8f32f145be7d80918dee29117372
Arun <arunkumarpg@hamaraindustries.xyz>
2023-12-25 10:42:03 +0530

    Added flake8 to dev requirements and included #noqa to settings.py for django generated settings code that is >79 chars. Added .flake8 config.

--- app/project/settings.py
+++ app/project/settings.py
@@ -92,16 +92,17 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator', # noqa: 501
+
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator', # noqa: 501
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator', # noqa: 501
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator', # noqa: 501
     },
 ]
 
